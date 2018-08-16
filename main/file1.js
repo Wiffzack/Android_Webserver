56a528194ef44f03091a4a237f7606ce316355a3
Wiffzack <wiffzackius@gmail.com>
2018-08-16 19:00:00 +0200

    Create file1.js

---
+++ main/file1.js
@@ -0,0 +1,14 @@
+$(function() {
+    var a = $("#contextMenu");
+    $("body").on("contextmenu", "table tr", function(b) {
+        a.css({
+            display: "block",
+            left: b.pageX,
+            top: b.pageY
+        });
+        return !1
+    });
+    $("html").click(function() {
+        a.hide()
+    })
+});
