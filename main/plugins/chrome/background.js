edcf129ea6ddb15829176073948e5e9ec09c04af
Wiffzack <wiffzackius@gmail.com>
2018-10-07 12:29:11 +0200

    Update background.js

--- main/plugins/chrome/background.js
+++ main/plugins/chrome/background.js
@@ -1,8 +1,32 @@
-var currentLocation;
-var currentURL;
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 var tabUrl;
 var keywords;
 var rating;
+var searchword;
+
+chrome.runtime.onInstalled.addListener(function() {
+	var contexts = ["page","selection","link","editable"];
+	var title = "Google Search";
+	chrome.contextMenus.create({
+		id: "Search",
+		title: "some title",
+		contexts: ["all"]
+	});
+});
+
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+    if (info.menuItemId == "Search") {
+		refresh();
+		mysearch();
+		if (searchword) {
+		getUrl();	
+    }
+	}
+});
+
+
 
 chrome.tabs.onUpdated.addListener(function() {
 	chrome.tabs.query({'active': true, 'lastFocusedWindow': true, 'currentWindow': true}, function (tabs) {tabUrl = tabs[0].url;});	
@@ -33,6 +57,24 @@ function sendCurrentUrl() {
 	//req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 	//req.send('url=' + encodeURIComponent(url));
 	}
+	
+function getUrl() {
+	if (window.XMLHttpRequest) {
+		xmlhttp=new XMLHttpRequest();
+	} else {
+		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+	}
+		xmlhttp.onreadystatechange=function() {
+	if (this.readyState==4 && this.status==200) {
+		//pass
+	}
+	}
+	window.open("http://127.0.0.1/search_plugin.php?keywords="+encodeURIComponent(searchword));
+	//window.location.replace("http://127.0.0.1/search_plugin.php?keywords="+encodeURIComponent(searchword));
+
+	}	
+	
+	
 
 function myFunction() {
     var txt;
@@ -49,13 +91,25 @@ function myFunction() {
 }
 }
 
+function mysearch() {
+    var txt;
+    searchword = prompt("Please enter search term:", "");
+    if (searchword == null || searchword == "") {
+        txt = "User cancelled the prompt.";
+    } else {
+		}
+}
+
 chrome.commands.onCommand.addListener(function(command) {
-	refresh();
-	myFunction();
-	//alert(tabUrl);
-	//console.log(window.tabUrl);
-	if (tabUrl) {
-	sendCurrentUrl();
+	if (command == "toggle-feature") {
+		refresh();
+		myFunction();
+		//console.log(window.tabUrl);
+		if (tabUrl) {
+		sendCurrentUrl();
+		}
+		console.log('onCommand event received for message: ', command);
 	}
-	console.log('onCommand event received for message: ', command);
 });
+
+
